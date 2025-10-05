3afd95483a2e21ad125a655faafd2d55e5f03a38
Sailesh T <saileshtrn06@gmail.com>
2025-10-05 21:03:14 +0530

    Initial Commit

---
+++ to-do-list/src/ToDoList.jsx
@@ -0,0 +1,42 @@
+import React , {useState} from 'react';
+
+function ToDoList(){
+
+    const [task,setTask] = useState([]);
+    const [newTask,setNewTask] = useState("");
+
+    function handleInputChange(){
+        setNewTask(event.target.value);
+    }
+
+    function addTask(){
+
+    }
+
+    function deleteTask(){
+
+    }
+
+    function moveTaskUp(){
+
+    }
+
+    function moveTaskDown(){
+
+    }
+    return(<>
+        <div className="to-do-list">
+            <h1>TO-DO-LIST</h1>
+
+            <input type='text'
+                   placeholder='Enter a task...'
+                   value={newTask}
+                   onChange={handleInputChange}>
+                   
+            </input>
+        </div>
+    
+    </>);
+}
+
+export default ToDoList;
