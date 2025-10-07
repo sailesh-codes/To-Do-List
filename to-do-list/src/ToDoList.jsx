d098420a3d4ce3c1da842ec34a48071c1d3f2254
Sailesh T <saileshtrn06@gmail.com>
2025-10-07 19:30:52 +0530

    updated code2

--- to-do-list/src/ToDoList.jsx
+++ to-do-list/src/ToDoList.jsx
@@ -10,7 +10,10 @@ function ToDoList(){
     }
 
     function addTask(){
-
+        if(newTask.trim() !== ""){
+            setTasks(t => [...t,newTask]);
+        setNewTask("");
+        }
     }
 
     function deleteTask(){
